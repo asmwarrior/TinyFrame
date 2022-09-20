da4210ad148fbb6ad7328be41bff900713e55f04
Marcel Nabeck <marcelnabeck@gmail.com>
2022-09-21 00:00:13 +0200

    crc code optimization

--- TinyFrame_CRC.hpp
+++ TinyFrame_CRC.hpp
@@ -4,7 +4,6 @@
 #include "TinyFrame_Types.hpp"
 
 namespace TinyFrame_n{
-//region Checksums
 
 // Custom checksum functions
 
@@ -41,42 +40,41 @@ CKSUM<CKSUM_TYPE> CksumEnd(CKSUM<CKSUM_TYPE> cksum);
 
 static constexpr uint8_t bit_reflect_lut[16U] = {
     /* OUT,    IN */
-    0b0000, // 0b0000
-    0b1000, // 0b0001
-    0b0100, // 0b0010
-    0b1100, // 0b0011
-    0b0010, // 0b0100
-    0b1010, // 0b0101
-    0b0110, // 0b0110
-    0b1110, // 0b0111
-    0b0001, // 0b1000
-    0b1001, // 0b1001
-    0b0101, // 0b1010
-    0b1101, // 0b1011
-    0b0011, // 0b1100
-    0b1011, // 0b1101
-    0b0111, // 0b1110
-    0b1111, // 0b1111
-    };
+    0b0000U, // 0b0000
+    0b1000U, // 0b0001
+    0b0100U, // 0b0010
+    0b1100U, // 0b0011
+    0b0010U, // 0b0100
+    0b1010U, // 0b0101
+    0b0110U, // 0b0110
+    0b1110U, // 0b0111
+    0b0001U, // 0b1000
+    0b1001U, // 0b1001
+    0b0101U, // 0b1010
+    0b1101U, // 0b1011
+    0b0011U, // 0b1100
+    0b1011U, // 0b1101
+    0b0111U, // 0b1110
+    0b1111U, // 0b1111
+};
 
-static constexpr uint8_t reflect_bits(uint8_t byte) {
-   return (bit_reflect_lut[byte & 0b1111] << 4) | (bit_reflect_lut[byte >> 4]);
+static constexpr uint8_t reflect_bits(uint8_t byte)
+{
+    return (bit_reflect_lut[byte & 0b1111U] << 4U) | (bit_reflect_lut[byte >> 4U]);
 }
 
-static inline void reflect_bytes(uint8_t* p_start, size_t size, bool bit_reflect = false){
-    uint8_t* p_end = p_start + size - 1;
-    uint8_t temp = 0;
-    while(p_start <= p_end){
+static inline void reflect_bytes(uint8_t *p_start, size_t size, bool bit_reflect = false)
+{
+    uint8_t *p_end = p_start + (size - 1U);
+    uint8_t temp = 0U;
+    while (p_start <= p_end)
+    {
         temp = *p_start;
-        if(bit_reflect){
-            *p_start = reflect_bits(*p_end);
-            *p_end = reflect_bits(temp);
-        }else{
-            *p_start = *p_end;
-            *p_end = temp;
-        }
-        p_start++;
-        p_end--;
+        *p_start = bit_reflect ? reflect_bits(*p_end) : *p_end;
+        *p_end = bit_reflect ? reflect_bits(temp) : temp;
+
+        p_start += 1U;
+        p_end -= 1U;
     }
 }
 
@@ -86,7 +84,7 @@ constexpr static DATA_TYPE TablelessCrc_Function(DATA_TYPE poly, DATA_TYPE inita
 {
 
     /* extract relevant byte references */
-    DATA_TYPE msbMask = static_cast<DATA_TYPE>(1U << ((sizeof(DATA_TYPE) * 8U) - 1));
+    DATA_TYPE msbMask = static_cast<DATA_TYPE>(1U << ((sizeof(DATA_TYPE) * 8U) - 1U));
 
     /* init crc byte */
     inital_crc = inital_crc ^ (byte << ((sizeof(DATA_TYPE) - 1) * 8U));
@@ -94,21 +92,13 @@ constexpr static DATA_TYPE TablelessCrc_Function(DATA_TYPE poly, DATA_TYPE inita
     /* calculate byte CRC using XOR */
     for (uint8_t bitIndex = 0U; bitIndex < 8U; bitIndex++)
     {
-        if ((inital_crc & msbMask) != 0U)
-        {
-            inital_crc = (inital_crc << 1U) ^ poly;
-        }
-        else
-        {
-            inital_crc = (inital_crc << 1U);
-        }
+        inital_crc = (inital_crc << 1U) ^ (((inital_crc & msbMask) != 0U) ? poly : 0U);
     }
 
     /* return modified crc */
-    return inital_crc; 
+    return inital_crc;
 }
 
-//endregion
-}
+} // TinyFrame_n
 
 #endif // TinyFrame_CRC_HPP
