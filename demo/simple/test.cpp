c0549737a4be60253cc2c6ce0a34c3269e4accb9
asmwarrior <asmwarrior@gmail.com>
2024-04-30 11:17:57 +0800

    * support output color text in Windows console application, add a Code::Blocks project for the simple demo

--- demo/simple/test.cpp
+++ demo/simple/test.cpp
@@ -3,6 +3,10 @@
 #include "../../TinyFrame.hpp"
 #include "../utils.hpp"
 
+#ifdef _WIN32 // Windows specific code
+#include <Windows.h>
+#endif // _WIN32
+
 using namespace TinyFrame_n;
 using TinyFrame_Demo=TinyFrameDefault;
 
@@ -17,14 +21,22 @@ bool do_corrupt = false;
 void WriteImpl(const uint8_t *buff, size_t len)
 {
     printf("--------------------\n");
+
+#ifdef _WIN32 // Windows specific code
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN); // Set text color to green
+    printf("WriteImpl - sending frame:\n");
+    SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Reset text color to default
+#else // Linux specific code
     printf("\033[32mWriteImpl - sending frame:\033[0m\n");
-    
-    uint8_t *xbuff = (uint8_t *)buff;    
+#endif
+
+    uint8_t *xbuff = (uint8_t *)buff;
     if (do_corrupt) {
       printf("(corrupting to test checksum checking...)\n");
       xbuff[8]++;
     }
-    
+
     dumpFrame(xbuff, len);
 
     // Send it back as if we received it
@@ -81,11 +93,11 @@ int main(void)
     msg.len = 0;
     msg.type = 0x77;
     demo_tf.Query(&msg, testIdListener, nullptr, 0);
-    
+
     printf("This should fail:\n");
-    
+
     // test checksums are tested
-    do_corrupt = true;    
+    do_corrupt = true;
     msg.type = 0x44;
     msg.data = (pu8) "Hello2";
     msg.len = 7;
--- demo/utils.cpp
+++ demo/utils.cpp
@@ -5,7 +5,62 @@
 #include "utils.hpp"
 #include <cstdio>
 
+#ifdef _WIN32 // Windows specific code
+#include <Windows.h>
+#endif // _WIN32
+
 namespace TinyFrame_n{
+
+
+#ifdef _WIN32 // Windows specific code
+
+// Function to set the console text color
+void SetConsoleColor(WORD color)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
+
+// Helper function for testing
+void dumpFrame(const uint8_t* buff, size_t len)
+{
+    size_t i;
+    for (i = 0; i < len; i++)
+    {
+        printf("%3u ", buff[i]);
+        SetConsoleColor(FOREGROUND_BLUE | FOREGROUND_INTENSITY); // Set color to blue (foreground intensity)
+        printf("%02X", buff[i]);
+        SetConsoleColor(FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Reset color to default
+
+        if (buff[i] >= 0x20 && buff[i] < 127)
+        {
+            printf(" %c", buff[i]);
+        }
+        else
+        {
+            SetConsoleColor(FOREGROUND_RED); // Set color to red
+            printf(" .");
+            SetConsoleColor(FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Reset color to default
+        }
+        printf("\n");
+    }
+    printf("--- end of frame ---\n\n");
+}
+
+void dumpFrameInfo(Msg* msg)
+{
+    SetConsoleColor(FOREGROUND_GREEN | FOREGROUND_RED); // Set color to yellow (green + red = yellow)
+    printf("Frame info\n");
+    SetConsoleColor(FOREGROUND_GREEN); // Set color to green
+    printf("  type: 0x%Xh\n", (int)msg->type);
+    printf("  data: \"%.*s\"\n", (int)msg->len, msg->data);
+    printf("   len: 0x%X\n", (int)msg->len);
+    printf("    id: 0x%Xh\n", (int)msg->frame_id);
+    SetConsoleColor(FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Reset color to default
+    printf("\n");
+}
+
+#else // Linux system
 
 // helper func for testing
 void dumpFrame(const uint8_t *buff, size_t len)
@@ -33,4 +88,7 @@ void dumpFrameInfo(Msg *msg)
                "    id: 0x%Xh\033[0m\n\n",
            (int)msg->type, (int)msg->len, msg->data, (int)msg->len, (int)msg->frame_id);
 }
+
+#endif // _WIN32
+
 } // TinyFrame_n
